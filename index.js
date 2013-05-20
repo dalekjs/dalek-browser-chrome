3103ab4e895629f29a36356f6be98f8f67267e26
asciidisco <public@asciidisco.com>
2013-05-20 21:54:04 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+var Q = require('q');
+var spawn = require('child_process').spawn;
+var chromedriver = require('./lib/chromedriver');
+
+module.exports = {
+    port: 9002,
+    host: 'localhost',
+    path: '/',
+    spawned: null,
+    launch: function () {
+        var deferred = Q.defer();
+        var stream = '';
+        this.spawned = spawn(chromedriver.path, ['--port=9002']);
+
+        this.spawned.stdout.on('data', function (data) {
+            var dataStr = new String(data);
+            stream += dataStr;
+            if (stream.search('Started ChromeDriver') !== -1) {
+                deferred.resolve();
+            }
+        });
+        return deferred.promise;
+    },
+    kill: function () {
+        this.spawned.kill('SIGHUP');
+    }
+};
